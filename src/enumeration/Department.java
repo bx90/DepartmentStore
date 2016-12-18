e6bc28bd505b09b5f138e6db5121e8702154a162
PanJiayuaner <boxiong.sun@gmail.com>
2016-12-18 10:27:26 -0800

    Add enum type

---
+++ src/enumeration/Department.java
@@ -0,0 +1,31 @@
+package enumeration;
+
+/**
+ * The Enum class cannot be inherited, except for that 
+ * it can be treated as a normal class.
+ * 
+ * @author Boxiong
+ */
+public enum Department {
+	FOOD("Hungry?"),
+	LIQUOR("How old are you?"),
+	FASHION("You can never have too many fashions!"),
+	ELECTORIC("How much money do you have?");
+	
+	private String description;
+	//Private constructor.
+	private Department(String des) {
+		this.description = des;
+	}
+	public String getDescription() {
+		return this.description;
+	}
+	
+	// Enum can even have a Main function.
+	public static void main(String[] args) {
+		for (Department d : Department.values()) {
+			System.out.println(d.name() + ": "+d.getDescription());
+		}
+	}
+}
+
